0052710e2b28a9f8d88da0f3bd87a7bd4cfaef82
Paul Nykiel <paul.nykiel@gmail.com>
2020-12-01 22:01:31 +0100

    Added all files

---
+++ src/comm/Lobby.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file Lobby.cpp
+ * @author paul
+ * @date 01.12.20
+ * Description here TODO
+ */
+#include "Lobby.hpp"
---
+++ src/comm/Lobby.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file Lobby.hpp
+ * @author paul
+ * @date 01.12.20
+ * Description here TODO
+ */
+#ifndef SECRETBUNDESTAGSERVER_LOBBY_HPP
+#define SECRETBUNDESTAGSERVER_LOBBY_HPP
+
+namespace comm {
+    class Lobby {
+
+    };
+}
+
+#endif //SECRETBUNDESTAGSERVER_LOBBY_HPP
